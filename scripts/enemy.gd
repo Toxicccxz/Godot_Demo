58b4a7b98835913fe5dda345e7055eec0617fc37
Xavier Cui <toxicstrikercuixinzhou@gmail.com>
2025-09-13 03:25:11 -0600

    Create COMBAT SYSTEM

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,7 +4,13 @@ var SPEED = 40
 var player_chase = false
 var player = null
 
+var health = 100
+var player_inattact_zone = false
+var can_take_damage = true
+
 func _physics_process(delta: float) -> void:
+	deal_with_damage()
+	
 	if player_chase:
 		position += (player.position - position) / SPEED
 		
@@ -25,3 +31,30 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
+	
+func enemy():
+		pass
+
+
+func _on_enemy_hitbox_body_entered(body: Node2D):
+	if body.has_method("player"):
+		player_inattact_zone = true
+
+
+func _on_enemy_hitbox_body_exited(body: Node2D):
+	if body.has_method("player"):
+		player_inattact_zone = false
+
+func deal_with_damage():
+	if player_inattact_zone and Global.player_current_attact == true:
+		if can_take_damage == true:
+			health = health - 20
+			$take_danage_cooldown.start()
+			can_take_damage = false
+			print("slime health = ", health)
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_take_danage_cooldown_timeout():
+	can_take_damage = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,11 @@
 extends CharacterBody2D
 
+var enemy_inattack_range = false
+var enemy_attack_cooldown = true
+var health = 160
+var player_alive = true
+
+var attact_ip = false
 
 const SPEED = 100.0
 var current_dir = "none"
@@ -9,6 +15,14 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	player_movement(delta)
+	enemy_attack()
+	attack()
+	
+	if health <= 0:
+		player_alive = false
+		health = 0
+		print('player has been killed')
+		self.queue_free()
 
 func player_movement(delta: float) -> void:
 
@@ -48,22 +62,75 @@ func player_animation(movement):
 		if movement == 1:
 			animation.play("side_walk")
 		elif movement == 0:
-			animation.play("side_idle")
+			if attact_ip == false:
+				animation.play("side_idle")
 	if dir == "left":
 		animation.flip_h = true
 		if movement == 1:
 			animation.play("side_walk")
 		elif movement == 0:
-			animation.play("side_idle")
+			if attact_ip == false:
+				animation.play("side_idle")
 	if dir == "down":
 		animation.flip_h = false
 		if movement == 1:
 			animation.play("front_walk")
 		elif movement == 0:
-			animation.play("front_idle")
+			if attact_ip == false:
+				animation.play("front_idle")
 	if dir == "up":
 		animation.flip_h = false
 		if movement == 1:
 			animation.play("back_walk")
 		elif movement == 0:
-			animation.play("back_idle")
+			if attact_ip == false:
+				animation.play("back_idle")
+
+func player():
+	pass
+
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_inattack_range = true
+
+
+func _on_player_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_inattack_range = false
+		
+func enemy_attack():
+	if enemy_inattack_range and enemy_attack_cooldown == true:
+		health = health - 20
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+		print(health)
+		
+func _on_attack_cooldown_timeout():
+	enemy_attack_cooldown = true
+	
+func attack():
+	var dir = current_dir
+	
+	if Input.is_action_just_pressed("attack"):
+		Global.player_current_attact = true
+		attact_ip = true
+		if dir == "right":
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attact_timer.start()
+		if dir == "left":
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attact_timer.start()
+		if dir == "down":
+			$AnimatedSprite2D.play("front_attack")
+			$deal_attact_timer.start()
+		if dir == "up":
+			$AnimatedSprite2D.play("back_attack")
+			$deal_attact_timer.start()
+
+
+func _on_deal_attact_timer_timeout():
+	$deal_attact_timer.stop()
+	Global.player_current_attact = false
+	attact_ip = false
